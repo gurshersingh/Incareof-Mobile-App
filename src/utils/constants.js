cd1e28e3ccaa4e8ffe5a7800d2b1043ab8f6a9fe
Gursher Singh <gurshersingh@MacBook-Pro.local>
2020-08-08 22:39:17 +1000

    Final code front end

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const baseUrl = "http://192.168.1.113:2000/";
+export const route = {
+    JobSeekerProfile: ["JobSeekerProfile", "EditJobSeekerProfile"],
+    JobSeekerScreen: ["JobSeekerScreen", "EmployerProfile"],
+};
